3bd2b3b799784a34205fb7eca4640448fc749bff
jordones <jordon.e.smith@gmail.com>
2022-09-03 15:17:58 -0400

    Update UI font

--- RlFlappyBird/RlFlappyBird/src/states/countdownState.cpp
+++ RlFlappyBird/RlFlappyBird/src/states/countdownState.cpp
@@ -2,7 +2,7 @@
 #include "stateMachine.h"
 
 extern StateMachine gStateMachine;
-extern Font mediumFont;
+extern Font flappyFont;
 extern int WINDOW_HEIGHT;
 extern int WINDOW_WIDTH;
 
@@ -42,7 +42,7 @@ void CountdownState::Update(float dt)
 
 void CountdownState::Render()
 {
-  DrawTextEx(mediumFont, "CountdownState", {20, 30}, 14.0f, 1.0f, BLACK);
-  Vector2 textSize = MeasureTextEx(mediumFont, &countString, 14.0f, 1.0f);
-  DrawTextEx(mediumFont, &countString, {(float)WINDOW_WIDTH / 2 - textSize.x / 2, (float)WINDOW_HEIGHT / 2 - textSize.y / 2}, 14.0f, 1.0f, BLACK);
+  DrawTextEx(flappyFont, "CountdownState", {20, 30}, 28.0f, 1.0f, BLACK);
+  Vector2 textSize = MeasureTextEx(flappyFont, &countString, 28.0f, 1.0f);
+  DrawTextEx(flappyFont, &countString, {(float)WINDOW_WIDTH / 2 - textSize.x / 2, (float)WINDOW_HEIGHT / 2 - textSize.y / 2}, 28.0f, 1.0f, BLACK);
 }

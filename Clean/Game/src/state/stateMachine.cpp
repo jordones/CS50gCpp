511329cf6d2d4e4477d87393f38c2e1ad47e1a3b
Jordon Smith <jordon.e.smith@gmail.com>
2022-09-04 18:05:07 -0400

    Clean base project (#9)

---
+++ Clean/Game/src/state/stateMachine.cpp
@@ -0,0 +1,31 @@
+#include "state/stateMachine.h"
+
+StateMachine::StateMachine()
+{
+  current = factory.CreateState(Empty);
+}
+
+StateMachine::~StateMachine()
+{
+}
+
+void StateMachine::Change(StateName stateName, StateChangeParams params)
+{
+  IState *newState = factory.CreateState(stateName);
+  IState *oldState = current;
+
+  oldState->Exit();
+  delete oldState;
+  current = newState;
+  current->Enter(params);
+}
+
+void StateMachine::Update(float dt)
+{
+  current->Update(dt);
+}
+
+void StateMachine::Render()
+{
+  current->Render();
+}
